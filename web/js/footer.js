4c9870e29c6b0df379b06e37bf915cd7c0c2e142
ji-hwan <lutungerst@gmail.com>
2023-04-21 13:35:59 +0900

    230421 13:30 중간 백업

--- web/js/footer.js
+++ web/js/footer.js
@@ -1,6 +1,6 @@
 /* 4월 20일 상준 작성 */
 /* "만드는 중입니다~~" */
 const footerMenu = document.querySelector('#footer--right a');
-footerMenu.addEventListener("click", () => {
+footerMenu.addEventListener("mouseover", () => {
     alert("안녕하세요");
 });
