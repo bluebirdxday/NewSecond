4902619c6cd2d8b8ee8d62cf046d7e3544c667ab
ji-hwan <lutungerst@gmail.com>
2023-04-26 09:08:54 +0900

    2304025_로그인구현

--- web/js/login.js
+++ web/js/login.js
@@ -1,3 +1,7 @@
 function show(){
     document.querySelector('.modal--layout').classList.add('show');
 }
+
+function closeModal(){
+    document.querySelector('.modal--layout').classList.remove('show');
+}
