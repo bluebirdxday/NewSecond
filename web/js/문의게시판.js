7409eb8e9e3e79e5625077b15d4d0f159c597d1a
yumin0770 <122767407+yumin0770@users.noreply.github.com>
2023-04-28 11:29:25 +0900

    230428

--- web/js/문의게시판.js
+++ web/js/문의게시판.js
@@ -2,25 +2,13 @@ const qnaHead = document.querySelector('.qna_head');
   const qnaHeadContent = document.querySelector('.qna_head_content');
 
   qnaHead.addEventListener('click', function() {
-    if (qndHeadContent.style.display === 'none') {
-      qndHeadContent.style.display = 'block';
-      qndHeadContent.style.position = 'absolute'
-       
-    } else {
-      qndHeadContent.style.display = 'none';
+    if (qnaHeadContent.style.display === 'none') {
+      qnaHeadContent.style.display = 'block';
+      qnaHeadContent.style.position = 'absolute'
+         
+    }else {
+      qnaHeadContent.style.display = 'none';
      
-    }
+    } 
   });
 
-  const qnaLi= document.querySelectorAll('.qna_li');
-
-  for(let qnaList of qnaLi){
-
-  qnaList.addEventListener(()=>{
-    
-for(let i=0; i<qnaLi.length; i++){
-  qnaHead.innerText = qnaLi[i].innerText
-
-
-   }});
-}
