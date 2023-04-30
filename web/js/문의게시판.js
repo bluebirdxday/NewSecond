01cd4cbc361a19dfcc2ad1e35a836d0cfbc0f56f
yumin0770 <122767407+yumin0770@users.noreply.github.com>
2023-05-01 01:38:47 +0900

    230501 문의게시판,관심상품페이지,최근조회상품페이지

--- web/js/문의게시판.js
+++ web/js/문의게시판.js
@@ -1,15 +1,67 @@
 const qnaHead = document.querySelector('.qna_head');
   const qnaHeadContent = document.querySelector('.qna_head_content');
 
-  qnaHead.addEventListener('click', function() {
-    if (qnaHeadContent.style.display === 'none') {
-      qnaHeadContent.style.display = 'block';
-      qnaHeadContent.style.position = 'absolute'
-       
-    } else {
-      qnaHeadContent.style.display = 'none';
-     
+  window.onload=()=>{
+    document.querySelector('.qna_dropbtn_click').onclick = ()=>{
+        qna_dropdown();
     }
-  });
+    document.getElementsByClassName('qnaList').onclick = ()=>{
+      showMenu(value);
+    };
+    qna_dropdown = () => {
+      var v = document.querySelector('.qna_dropdown-content');
+      var qna_dropbtn = document.querySelector('.qna_dropbtn')
+      v.classList.toggle('show');
+      qna_dropbtn.style.borderColor = 'rgb(94, 94, 94)';
 
+    }
+
+    showMenu=(value)=>{
+      var qna_dropbtn_content = document.querySelector('.qna_dropbtn_content');
+      var qna_dropbtn_click = document.querySelector('.qna_dropbtn_click');
+      var qna_dropbtn = document.querySelector('.qna_dropbtn');
+
+     qna_dropbtn_content.innerText = '';
+      qna_dropbtn_content.innerText = value;
+      qna_dropbtn_content.style.color = '#005EEE';
+      qna_dropbtn.style.borderColor = '#005EEE';
+      
+    }
+  }
+  window.onclick= (e)=>{
+    if(!e.target.matches('.qna_dropbtn_click')){
+      var qna_dropdowns = document.getElementsByClassName("qna_dropdown-content");
+
+      var qna_dropbtn_icon = document.querySelector('.qna_dropbtn_icon');
+      var qna_dropbtn_content = document.querySelector('.qna_dropbtn_content');
+      var qna_dropbtn_click = document.querySelector('.qna_dropbtn_click');
+      var qna_dropbtn = document.querySelector('.qna_dropbtn');
+
+      var i;
+      for (i = 0; i < qna_dropdowns.length; i++) {
+        var openDropdown = qna_dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  /* 모달 */
+  const qnaBtn = document.getElementById('qna_button');
+  const modal = document.getElementById('qna_modal');
+  const qnaCloseBtn = document.getElementById('qna_closeBtn');
   
+  qnaBtn.onclick = function() {
+    modal.style.display = 'block';
+    modal.style.position = 'fixed';
+  }
+  qnaCloseBtn.onclick = function() {
+    modal.style.display = 'none';
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
