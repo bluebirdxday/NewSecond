47c442753dd4043c5e03d90a0f16acb6c363986f
ji-hwan <lutungerst@gmail.com>
2023-05-11 16:01:26 +0900

    jdbc 임시 접속 성공 및 db에서 로그인된 정보 꺼내오기 성공

--- newSecond/src/main/java/project/kh/newsecond/user/controller/UserController.java
+++ newSecond/src/main/java/project/kh/newsecond/user/controller/UserController.java
@@ -2,6 +2,7 @@ package project.kh.newsecond.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class UserController {
 	private UserService service;
 	
 	@PostMapping("/login")
-	public String login(User inputUser
+	public String login(User inputUser, Model model
 						,@RequestHeader(value="referer") String referer
 						,RedirectAttributes ra) {
 		
@@ -32,7 +33,8 @@ public class UserController {
 		if(loginUser != null) {
 			
 			path += "/";
-			
+			model.addAttribute("loginUser", loginUser);
+			System.out.println(loginUser);
 		} else {
 			path += referer;
 			ra.addFlashAttribute("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
