670d7772ecd62199497f093a07ec9e9843d04787
sangzoon0102 <sangzoon0102@naver.com>
2023-05-16 20:57:08 +0900

    20230516_BE MODEL2 오류없음확인_16일 학원에서 마지막 커밋

--- newSecond/src/main/java/project/kh/newsecond/chatting/model/service/ChattingServiceImpl.java
+++ newSecond/src/main/java/project/kh/newsecond/chatting/model/service/ChattingServiceImpl.java
@@ -1,5 +1,13 @@
 package project.kh.newsecond.chatting.model.service;
 
-public class ChattingServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import project.kh.newsecond.chatting.model.dao.ChattingDAO;
+
+@Service
+public class ChattingServiceImpl implements ChattingService {
+
+	@Autowired
+	private ChattingDAO dao;
 }
--- newSecond/src/main/java/project/kh/newsecond/priceView/model/service/PriceViewImpl.java
+++ newSecond/src/main/java/project/kh/newsecond/priceView/model/service/PriceViewImpl.java
@@ -1,5 +1,13 @@
 package project.kh.newsecond.priceView.model.service;
 
-public class PriceViewImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import project.kh.newsecond.priceView.model.dao.PriceViewDAO;
+
+@Service
+public class PriceViewImpl implements PriceView {
+
+	@Autowired
+	private PriceViewDAO dao;
 }
