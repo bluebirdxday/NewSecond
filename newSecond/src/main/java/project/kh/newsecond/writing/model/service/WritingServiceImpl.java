7bde224f30f47d4b3c868f01006cd3b0238fdbb3
sangzoon0102 <sangzoon0102@naver.com>
2023-05-25 21:25:39 +0900

    발표전_신규회원이라도글쓰기가능한코드추가@@@@중요하니머지필수!

--- newSecond/src/main/java/project/kh/newsecond/writing/model/service/WritingServiceImpl.java
+++ newSecond/src/main/java/project/kh/newsecond/writing/model/service/WritingServiceImpl.java
@@ -93,13 +93,20 @@ public class WritingServiceImpl implements WritingService {
 					int tempIndex = FinalImages.get(i).getFileOrder();
 					int index = tempIndex - 1;
 					
+					String afterRename = FinalImages.get(i).getFileName();
 					// 글 올리기 전에 폴더가 없다면 폴더 미리 만드는 코드 추가
-					
+					String userFolderPath = filePath + writing.getUserNo() + "/";
+					File userFolder = new File(userFolderPath);
+
+					if (!userFolder.exists()) {
+					    userFolder.mkdirs(); // 필요한 모든 상위 디렉토리를 생성합니다.
+					}
+
+					images.get(index).transferTo(new File(userFolderPath + afterRename));
 					
 					
 					// 파일로 변환
-					String afterRename = FinalImages.get(i).getFileName();
-					images.get(index).transferTo(new File(filePath + writing.getUserNo() + "/" + afterRename));
+//					images.get(index).transferTo(new File(filePath + writing.getUserNo() + "/" + afterRename));
 				
 				}
 				
